35e10907e85695fbc1c3a054602d28d6b7918fb7
David Brennan <david.n.brennan@gmail.com>
2015-11-02 09:42:30 +0100

    Removed reference to mira-bookmarks as deleted that instance

--- mira-examples/js/my-globals.js
+++ mira-examples/js/my-globals.js
@@ -2,9 +2,9 @@
 switch(window.location.protocol) {
   // set root_url depending on whether running locally or not
   case 'file:':
-    var root_url = "http://localhost:3000/api/projects/2";
+    var root_url = "http://localhost:3000/api/projects/5";
     break;
-  default: 
+  default:
     // var root_url = "http://46.101.208.152/api/projects/5";
     var root_url = "http://178.62.193.189/api/projects/6";
 }
@@ -39,15 +39,15 @@ function htmlFunc(func) {
 // http://stackoverflow.com/a/979325/1002140
 var sort_by = function(field, reverse, primer){
 
-   var key = primer ? 
-       function(x) {return primer(x[field])} : 
+   var key = primer ?
+       function(x) {return primer(x[field])} :
        function(x) {return x[field]};
 
    reverse = !reverse ? 1 : -1;
 
    return function (a, b) {
        return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
-     } 
+     }
 }
 
 
